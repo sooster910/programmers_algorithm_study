a44e1f54a85570e1f06d4ebdee51610bd84642f9
Hyunsu Joo <hs9880@gmail.com>
2021-05-11 23:02:52 +0900

    👋 ADD: programmers 타겟넘버 js ver.

---
+++ DFS/BFS/targetNumber.js
@@ -0,0 +1,23 @@
+function solution(numbers, target) {
+    
+    let sum=0, index=0
+    dfs(numbers,target,sum,index)
+    console.log(answer)
+    return answer;
+
+}
+function dfs(numbers, target, sum, index){
+    //기저조건 1. index 가 number보다 크거나 같으면 2. target이 sum과같으면
+    if(index>= numbers.length){
+        if(target === sum) answer++;            
+        return
+    }
+       //+ dfs
+       dfs(numbers, target, sum+numbers[index], index+1);
+       //-연산 dfs
+       dfs(numbers,target,sum-numbers[index],index+1);
+    
+    
+}
+var answer = 0;
+solution([1, 1, 1, 1], 2)
