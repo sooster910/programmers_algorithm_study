b8327905ec58f9e1d63a5cc9520e5aa0bb80adee
Hyunsu Joo <hs9880@gmail.com>
2021-06-15 16:59:27 +0900

    👋 ADD : programmers H-Index & testCase (ver. js)

---
+++ src/H-index/index.js
@@ -0,0 +1,23 @@
+function solution(citations) {
+
+    let sortedCitations = citations.sort((a,b)=>a-b);
+    let h_index =1;
+    let max_h_index = 0;
+
+    while(h_index<=sortedCitations.length){
+        let count = 0;
+
+        for(let citation of sortedCitations){
+            //만약 citation이 h_index 이상 인용되었으면 count
+            if(citation >=h_index) count++;
+            if(count >=h_index){ //인용된 횟수(h_index)를 만족하는 인용된 논문의 개수가 h_index를 만족한다면
+                max_h_index = count>max_h_index?count:max_h_index;
+                break
+            }
+        }
+        h_index++;
+    }
+    return max_h_index;
+}
+
+module.exports={solution};
